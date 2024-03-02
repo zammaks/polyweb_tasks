93eb09c3c7cd4de104918287b1793d8fc4727b0d
zammaks <zam.maks2005@gmail.com>
2024-03-03 01:10:37 +0300

    adding props

--- my-app/src/Components/main/Main.jsx
+++ my-app/src/Components/main/Main.jsx
@@ -4,15 +4,54 @@ import Logos from './Logos';
 import Promotion from './Promotion';
 import News from './News';
 import '../../App.css';
+
+const newsData = [
+    {
+        link: '/news1',
+        image: require('../../img/new1.png'),
+        starImage: require('../../img/star.svg'),
+        title: 'LIB TECH',
+        description: 'Мужской сноуборд',
+        price: 34392,
+    },
+    {
+        link: '/news1',
+        image: require('../../img/new1.png'),
+        starImage: '../../img/white star.svg',
+        title: 'LIB TECH',
+        description: 'Мужской сноуборд',
+        price: 34392,
+        discount: '-50%',
+
+    },
+    {
+        link: '/news1',
+        image: require('../../img/new1.png'),
+        starImage: '../../img/white star.svg',
+        title: 'LIB TECH',
+        description: 'Мужской сноуборд',
+        price: 34392,
+    },
+    {
+        link: '/news1',
+        image: require('../../img/new1.png'),
+        starImage: '../../img/white star.svg',
+        title: 'LIB TECH',
+        description: 'Мужской сноуборд',
+        price: 34392,
+    },
+];
+
 function Main() {
     return (
         <main>
             <Sale/>
             <Logos/>
             <Promotion/>
-            <News/>
+            <News newsData={newsData} />
         </main>
     );
 }
+
 
 export default Main;
--- my-app/src/Components/main/News.jsx
+++ my-app/src/Components/main/News.jsx
@@ -1,84 +1,58 @@
 import React from 'react';
-import ArrowRight from '../../img/arrow-right.svg';
-import ArrowLeft from '../../img/arrow-left.svg';
+import PropTypes from 'prop-types';
 import Star from '../../img/star.svg';
-import WhiteStar from '../../img/white star.svg';
-function News() {
+import ArrowLeft from '../../img/arrow-left.svg';
+import ArrowRight from '../../img/arrow-right.svg';
+
+
+function News(props) {
+    const { newsData } = props;
     return (
         <section className="news">
             <div className="news__wrapper">
                 <h3 className="news__title">Новинки</h3>
-                <button className="news__scroll-left">
-                    <img src={ArrowLeft} alt="влево" className="news__arrow-left" />
+                <button class="sale__scroll-left">
+                    <img src={ArrowLeft} alt="влево" className="sale__arrow-left"/>
                 </button>
                 <ul className="news__list">
-                    <li className="news__card">
-                        <a href="" className="news__card-link">
-                            <div className="card__img-wrapper">
-                                <img src={require('../../img/new1.png')} alt="" className="card__img" />
-                                <button className="card__star-wrapper star-black">
-                                    <img src={WhiteStar} alt="" className="card__star-img" />
-                                </button>
-                            </div>
-                            <h4 className="card__title">LIB TECH</h4>
-                            <p className="card__description">Мужской сноуборд</p>
-                            <p className="card__price">34392 ₽</p>
-                        </a>
-                    </li>
-                    <li className="news__card">
-                        <a href="" className="news__card-link">
-                            <div className="card__img-wrapper">
-                                <img src={require('../../img/new2.png')} alt="" className="card__img" />
-                                <button className="card__star-wrapper">
-                                    <img src={Star} alt="" className="card__star-img" />
-                                </button>
-                                <span className="card__sale">-50%</span>
-                            </div>
-                            <h4 className="card__title">LIB TECH</h4>
-                            <p className="card__description">Мужской сноуборд</p>
-                            <p className="card__price old-price">34392 ₽</p>
-                            <p className="card__price new-price">17392 ₽</p>
-                        </a>
-                    </li>
-                    <li className="news__card">
-                        <a href="" className="news__card-link">
-                            <div className="card__img-wrapper">
-                                <img src={require('../../img/new3.png')} alt="" className="card__img" />
-                                <button className="card__star-wrapper">
-                                    <img src={Star} alt="" className="card__star-img" />
-                                </button>
-                            </div>
-                            <h4 className="card__title">LIB TECH</h4>
-                            <p className="card__description">Мужской сноуборд</p>
-                            <p className="card__price">34392 ₽</p>
-                        </a>
-                    </li>
-                    <li className="news__card">
-                        <a href="" className="news__card-link">
-                            <div className="card__img-wrapper">
-                                <img src={require('../../img/new4.png')} alt="" className="card__img" />
-                                <button className="card__star-wrapper">
-                                    <img src={Star} alt="" className="card__star-img" />
-                                </button>
-                            </div>
-                            <h4 className="card__title">LIB TECH</h4>
-                            <p className="card__description">Мужской сноуборд</p>
-                            <p className="card__price">34392 ₽</p>
-                        </a>
-                    </li>
+                    {newsData.map((item, index) => (
+                        <li key={index} className="news__card">
+                            <a href={item.link} className="news__card-link">
+                                <div className="card__img-wrapper">
+                                    <img src={item.image} alt="" className="card__img" />
+                                    <button className="card__star-wrapper">
+                                        <img src={Star} alt="" className="card__star-img" />
+                                    </button>
+                                    {item.discount && <span className="card__sale">{item.discount}</span>}
+                                </div>
+                                <h4 className="card__title">{item.title}</h4>
+                                <p className="card__description">{item.description}</p>
+                                <p className={`card__price ${item.discount ? 'old-price' : 'new-price'}`}>
+                                    {item.price} ₽
+                                </p>
+                            </a>
+                        </li>
+                    ))}
                 </ul>
-                <button className="news__scroll-right">
-                    <img
-                        src={ArrowRight}
-                        alt="вправо"
-                        className="news__arrow-right"
-                    />
+                <button class="news__scroll-right">
+                    <img src={ArrowRight} alt="вправо" className="news__arrow-right"/>
                 </button>
-                <a href="" className="news__show-more">
-                    Показать больше
-                </a>
+                <a href="" class="news__show-more">Показать больше</a>
             </div>
         </section>
     );
 }
+
+News.propTypes = {
+    newsData: PropTypes.arrayOf(PropTypes.shape({
+        link: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        starImage: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        discount: PropTypes.string,
+    })).isRequired,
+};
+
 export default News;
